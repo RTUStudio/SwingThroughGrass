1780669752655d2be55e442af92edf4186f64b02
IPECTER 이팩터 <80433772+IPECTER@users.noreply.github.com>
2025-06-04 23:00:08 +0900

    SwingThroughGrass 1.1.6 Fix: Interact entity through block

--- src/main/java/com/github/ipecter/rtustudio/stg/listener/PlayerInteract.java
+++ src/main/java/com/github/ipecter/rtustudio/stg/listener/PlayerInteract.java
@@ -7,6 +7,7 @@ import kr.rtuserver.framework.bukkit.api.platform.MinecraftVersion;
 import org.bukkit.FluidCollisionMode;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
@@ -28,38 +29,44 @@ public class PlayerInteract extends RSListener<SwingThroughGrass> {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onSwing(PlayerInteractEvent e) {
-        if (e.getAction() == Action.LEFT_CLICK_BLOCK) {
-            Block block = e.getClickedBlock();
-            if (block == null) return;
+    private void onSwing(PlayerInteractEvent e) {
+        if (e.getAction() != Action.LEFT_CLICK_BLOCK) return;
+        Block block = e.getClickedBlock();
+        if (block == null) return;
 
-            if (!blockConfig.isCollidable()) {
-                if (isPaper) {
-                    if (block.getType().isSolid() || block.isCollidable()) return;
-                } else {
-                    if (block.getType().isSolid() || !block.isPassable()) return;
-                }
-            }
-
-            boolean pass = blockConfig.getMaterials().contains(block.getType());
-            if (blockConfig.getMode() == BlockConfig.Mode.BLACKLIST) pass = !pass;
-            if (!pass) return;
-
-            Player player = e.getPlayer();
-            RayTraceResult result;
-            if (isPaper && !player.isInsideVehicle() && MinecraftVersion.isSupport("1.19.3")) {
-                result = player.rayTraceEntities(3, true);
+        if (!blockConfig.isCollidable()) {
+            if (isPaper) {
+                if (block.getType().isSolid() || block.isCollidable()) return;
             } else {
-                result = player.getWorld().rayTrace(player.getEyeLocation(), player.getLocation().getDirection(),
-                        3, FluidCollisionMode.NEVER, true, 0,
-                        entity -> entity != player);
+                if (block.getType().isSolid() || !block.isPassable()) return;
             }
+        }
 
-            if (result != null && result.getHitEntity() != null) {
-                PlayerInteractEvent event = PlayerInteractEventFactory.create(newerVersion, e, result, player);
-                if (event.callEvent()) player.attack(result.getHitEntity());
+        boolean pass = blockConfig.getMaterials().contains(block.getType());
+        if (blockConfig.getMode() == BlockConfig.Mode.BLACKLIST) pass = !pass;
+        if (!pass) return;
+
+        Player player = e.getPlayer();
+        RayTraceResult result;
+        if (isPaper && !player.isInsideVehicle() && MinecraftVersion.isSupport("1.19.3")) {
+            result = player.rayTraceEntities(3, true);
+        } else {
+            result = player.getWorld().rayTrace(player.getEyeLocation(), player.getLocation().getDirection(),
+                    3, FluidCollisionMode.NEVER, true, 0,
+                    entity -> entity != player);
+        }
+        if (result == null) return;
+        if (result.getHitEntity() != null) {
+            PlayerInteractEvent event = PlayerInteractEventFactory.create(newerVersion, e, result, player);
+            if (callEvent(event)) {
+                player.attack(result.getHitEntity());
                 if (!blockConfig.isDestroy()) e.setCancelled(true);
             }
         }
+    }
+
+    private boolean callEvent(PlayerInteractEvent event) {
+        org.bukkit.Bukkit.getPluginManager().callEvent(event);
+        return event.useItemInHand() != Event.Result.DENY;
     }
 }
